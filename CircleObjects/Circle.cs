a7387c1da8e07b2a6050d54645fef198fb0c5507
Chris <cmangotti3@gmail.com>
2023-04-24 08:48:25 -0400

    first commit

---
+++ CircleObjects/Circle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjects
+{
+    internal class Circle
+    {
+        private double radius { get; set; }
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double CalculateDiameter()
+        {
+            return 2 * radius;
+        }
+        public double CalculateCircumference()
+        {
+            return 2 * radius * Math.PI;
+        }
+        public double CalculateArea()
+        {
+            return radius * radius * Math.PI;
+        }
+        public void Grow()
+        {
+            Console.WriteLine("Should the circle grow? (y/n): ");
+            string input = Console.ReadLine().Trim().ToLower();
+            if (input == "y")
+            {
+                radius = 2 * radius;
+                Console.WriteLine("Diameter: " + CalculateDiameter());
+                Console.WriteLine("Circumference: " + CalculateCircumference());
+                Console.WriteLine("Area: " + CalculateArea());
+                Console.WriteLine();
+                Grow();
+            }
+            else if (input == "n")
+            {
+                Console.WriteLine("Goodbye. The circle's final radius is " + radius);
+            }
+            else
+            {
+                Console.WriteLine("I didn't understand that, lets try again.");
+                Grow();
+            }
+        }
+        public double GetRadius()
+        {
+            try
+            {
+                Console.WriteLine("Enter Radius: ");
+                radius = double.Parse(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Invalid input: Not a number, try again.");
+                GetRadius();
+            }
+            return radius;
+        }
+    }
+}
---
+++ CircleObjects/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CircleObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Circle Tester");
+            Circle circle = new Circle(0);
+            circle.GetRadius();
+            Console.WriteLine("Diameter: " + circle.CalculateDiameter());
+            Console.WriteLine("Circumference: " + circle.CalculateCircumference());
+            Console.WriteLine("Area: " + circle.CalculateArea());
+            Console.WriteLine();
+            circle.Grow();
+        }
+    }
+}
